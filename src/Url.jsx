ed1c42bbe9e655430dc9f716dc9c5176cc853fe4
makiichikawa <punkfolkfolk@gmail.com>
2025-03-07 23:18:17 +0900

    tagをpull downで選べるようにする

--- src/Url.jsx
+++ src/Url.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import Select from 'react-select'
 import './Url.css'
 
 const Url = ({url, handDeleteUrl}) => {
   const tag = url.tags[0]
+  const options = [
+    { value: 'rice', label: 'ごはんもの' },
+    { value: 'main', label: '主菜' },
+    { value: 'side', label: '副菜' },
+    { value: 'another', label: 'その他' }
+  ]
+  const formatOptionLabel = (data, meta) => {
+    return meta.context === 'value' ? (
+      <div className={`tag ${data.value}`}>{data.label}</div>
+    ) : (
+      <div className={`tag ${data.value}`}>{data.label}</div>
+    )
+  }
   return (
     <div style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '5px', margin: '10px 0' }}>
-      <div className={`tag ${tag.className}`}>{tag.name}</div>
+      <Select
+        defaultValue={{ value: tag.className, label: tag.name }}
+        options={options}
+        formatOptionLabel={formatOptionLabel}
+      />
+      {/* <div className={`tag ${tag.className}`}>{tag.name}</div> */}
       <a href={url.url} style={{ color: '#007BFF', textDecoration: 'underline' }}>{url.title}</a>
       <button onClick={() => handDeleteUrl(url.id)} style={{ marginLeft: '10px', backgroundColor: '#ccc', color: '#fff' }}>削除</button>
     </div>
